6b45e7516484722a3d16897b03e713538413d1b8
Gonzalo Techera <techera.gonzalo@gmail.com>
2018-04-12 16:39:10 -0300

    Adding Routes

--- app.js
+++ app.js
@@ -2,8 +2,14 @@ const express = require("express");
 const exphbs = require("express-handlebars");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
+const session = require("express-session");
+const flash = require("connect-flash");
 
 const app = express();
+
+//Load Routes
+const ideas = require("./routes/ideas");
 
 //Connect to mongose
 mongoose
@@ -15,10 +21,6 @@ mongoose
     console.log(error);
   });
 
-//Load Idea Model
-require("./models/Idea");
-const Idea = mongoose.model("ideas");
-
 //Handlebars Middleware
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
@@ -26,6 +28,29 @@ app.set("view engine", "handlebars");
 // parse application/x-www-form-urlencoded body-parser
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+//Method-override Middleware
+app.use(methodOverride("_method"));
+
+//Express-Session Middleware
+app.use(
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+//Connect-Flash Middleware
+app.use(flash());
+
+//Global Variables
+app.use(function(req, res, next) {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash("error");
+  next();
+});
 
 //Index Route
 app.get("/", (req, res) => {
@@ -38,59 +63,18 @@ app.get("/about", (req, res) => {
   res.render("about");
 });
 
-//Ideas Index Page
-app.get("/ideas", (req, res) => {
-  Idea.find({})
-    .sort({ date: "desc" })
-    .then(ideas => {
-      res.render("ideas/index", { ideas: ideas });
-    });
+//User Login Route
+app.get("/user/login", (req, res) => {
+  res.send("Login");
 });
 
-//Add Idea Form
-app.get("/ideas/add", (req, res) => {
-  res.render("ideas/add");
+//User Register Route
+app.get("/user/register", (req, res) => {
+  res.send("Register");
 });
 
-//Edit Idea Form
-app.get("/ideas/edit/:id", (req, res) => {
-  Idea.findOne({
-    _id: req.params.id
-  }).then(idea => {
-    res.render("ideas/edit", {
-      idea: idea
-    });
-  });
-});
-
-//Process Form
-app.post("/ideas", (req, res) => {
-  let errors = [];
-  if (!req.body.title) {
-    errors.push("Por favor ingrese un título");
-  }
-  if (!req.body.details) {
-    errors.push("Por favor ingrese los detalles");
-  }
-  if (errors.length > 0) {
-    res.render("ideas/add", {
-      errors: errors,
-      title: req.body.title,
-      details: req.body.details
-    });
-  } else {
-    const newUser = {
-      title: req.body.title,
-      details: req.body.details
-    };
-    new Idea(newUser)
-      .save()
-      .then(idea => {
-        res.redirect("/ideas");
-      })
-      .catch();
-  }
-});
+//Use Routes
+app.use("/ideas", ideas);
 
 const port = 5000;
 
---
+++ routes/ideas.js
@@ -0,0 +1,86 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const router = express.Router();
+
+//Load Idea Model
+require("../models/Idea");
+const Idea = mongoose.model("ideas");
+
+//"Index Page
+router.get("/", (req, res) => {
+  Idea.find({})
+    .sort({ date: "desc" })
+    .then(ideas => {
+      res.render("ideas/index", { ideas: ideas });
+    });
+});
+
+//Add Idea Form
+router.get("/add", (req, res) => {
+  res.render("ideas/add");
+});
+
+//Edit Idea Form
+router.get("/edit/:id", (req, res) => {
+  Idea.findOne({
+    _id: req.params.id
+  }).then(idea => {
+    res.render("ideas/edit", {
+      idea: idea
+    });
+  });
+});
+
+//Process Form
+router.post("/", (req, res) => {
+  let errors = [];
+  if (!req.body.title) {
+    errors.push("Por favor ingrese un título");
+  }
+  if (!req.body.details) {
+    errors.push("Por favor ingrese los detalles");
+  }
+  if (errors.length > 0) {
+    res.render("ideas/add", {
+      errors: errors,
+      title: req.body.title,
+      details: req.body.details
+    });
+  } else {
+    const newUser = {
+      title: req.body.title,
+      details: req.body.details
+    };
+    new Idea(newUser).save().then(idea => {
+      req.flash("success_msg", "Video idea added");
+      res.redirect("/");
+    });
+  }
+});
+
+//Edit Form Process
+router.put("/:id", (req, res) => {
+  //update idea
+  Idea.findOne({
+    _id: req.params.id
+  }).then(idea => {
+    //Nuevos valores
+    idea.title = req.body.title;
+    idea.details = req.body.details;
+    idea.save().then(idea => {
+      req.flash("success_msg", "Video Idea Updated");
+      res.redirect("/");
+    });
+  });
+});
+
+//Delete Idea
+
+router.delete("/:id", (req, res) => {
+  Idea.remove({ _id: req.params.id }).then(() => {
+    req.flash("success_msg", "Video Idea Removed");
+    res.redirect("/");
+  });
+});
+
+module.exports = router;
